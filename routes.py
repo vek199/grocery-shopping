8b3628d87594fc530f82be8fd408d76c5823dc7f
vek199 <vekash1992002@gmail.com>
2024-08-16 16:30:06 +0530

    added  auth required decorator and profile page
    added auth required dec and profile page

--- routes.py
+++ routes.py
@@ -1,10 +1,24 @@
-from flask import render_template, request, url_for, flash, redirect
+from flask import render_template, request, url_for, flash, redirect, session
 from app import app
 from models import db, User, Category, Cart, Product, Transaction, Order
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else: 
+            flash('Please login to continue')
+            return redirect(url_for('login'))
+    return inner
+
 @app.route('/')
+@auth_required
 def index():
-    return render_template('index.html')
+        return render_template('index.html')
+
 
 
 @app.route('/login')
@@ -30,6 +44,8 @@ def login_post():
         flash("Incorrect password")
         return redirect(url_for('login'))
     
+    session['user_id'] = user.id
+    flash('User Successfully logged in')
     return redirect(url_for('index'))
 
 @app.route('/register')
@@ -63,4 +79,49 @@ def register_post():
     db.session.commit()
     flash('Done')
     return redirect(url_for('login'))
+
+@app.route('/profile')
+@auth_required
+def profile():
+        user = User.query.get(session['user_id'])
+        return render_template('profile.html', user=user)
     
+
+@app.route('/profile', methods=['POST'])
+@auth_required
+def profile_post():
+    username = request.form.get('Username')
+    cpassword = request.form.get('cpassword')
+    password = request.form.get('password')
+    name = request.form.get('name')
+    
+    if not username or not cpassword or not password:
+        flash('please fill all the required fields')
+        return redirect(url_for('profile'))
+    
+    user = User.query.get(session('user_id'))
+    
+    if not check_password_hash(user.passhash, cpassword):
+        flash('Incorrect Current Password')
+        return redirect(url_for('profile'))
+    
+    if username != user.username:
+        new_username = User.query.filter_by(username=username).first()
+        if new_username:
+            flash('Username already exists')
+            return redirect(url_for('profile'))
+        
+    new_password_hash = generate_password_hash(password)
+    user.username = username
+    user.passhash = new_password_hash
+    user.name = name
+    db.session.commit()
+    flash('Profile Updated successfully')
+    return redirect(url_for('profile'))
+        
+
+@app.route('/logout')
+@auth_required
+def logout():
+    session.pop('user_id')
+    return redirect(url_for('login'))
